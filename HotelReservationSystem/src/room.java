040f433688847be0fd588ceca90f2dabebe8051d
preethi1918 <preethiravi981@gmail.com>
2026-01-09 09:20:54 +0530

    Update room.java

--- HotelReservationSystem/src/room.java
+++ HotelReservationSystem/src/room.java
@@ -1,1 +1,14 @@
+public class Room {
+    int roomNumber;
+    String type;
+    double price;
+    boolean isBooked;
+
+    public Room(int roomNumber, String type, double price) {
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.price = price;
+        this.isBooked = false;
+    }
+}
 
