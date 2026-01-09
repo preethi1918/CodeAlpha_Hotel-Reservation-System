b80a315b6e41741c2f496cd0ffdafb43c82334a1
preethi1918 <preethiravi981@gmail.com>
2026-01-09 09:21:38 +0530

    Update filemanager.java

--- HotelReservationSystem/src/filemanager.java
+++ HotelReservationSystem/src/filemanager.java
@@ -1,1 +1,15 @@
+import java.io.*;
+
+public class FileManager {
+
+    public static void saveBooking(String data) {
+        try {
+            FileWriter fw = new FileWriter("bookings.txt", true);
+            fw.write(data + "\n");
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("File Error!");
+        }
+    }
+}
 
