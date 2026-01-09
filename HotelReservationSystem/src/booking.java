e24e8c6f6f8a399d4b00383afa58e38ba5a87c1d
preethi1918 <preethiravi981@gmail.com>
2026-01-09 09:21:16 +0530

    Update booking.java

--- HotelReservationSystem/src/booking.java
+++ HotelReservationSystem/src/booking.java
@@ -1,1 +1,10 @@
+public class Booking {
+    String customerName;
+    int roomNumber;
+
+    public Booking(String customerName, int roomNumber) {
+        this.customerName = customerName;
+        this.roomNumber = roomNumber;
+    }
+}
 
