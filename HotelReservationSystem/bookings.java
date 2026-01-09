639f565ebd027da52bb3dd49b344da433dea5f3f
preethi1918 <preethiravi981@gmail.com>
2026-01-09 09:24:38 +0530

    Update bookings.java

--- HotelReservationSystem/bookings.java
+++ HotelReservationSystem/bookings.java
@@ -1,1 +1,35 @@
+public class Booking {
+    private String customerName;
+    private int roomNumber;
+    private String roomType;
+    private double amountPaid;
+
+    public Booking(String customerName, int roomNumber, String roomType, double amountPaid) {
+        this.customerName = customerName;
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.amountPaid = amountPaid;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public double getAmountPaid() {
+        return amountPaid;
+    }
+
+    @Override
+    public String toString() {
+        return customerName + "," + roomNumber + "," + roomType + "," + amountPaid;
+    }
+}
 
